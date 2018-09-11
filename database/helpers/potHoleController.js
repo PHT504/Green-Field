450c213ac6fcc6610626267b760e5283aba8e972
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-09-11 12:20:27 -0500

    renamed indexjs in the database to potholes to make more sense and created to controllers for the two models that currently just require them

---
+++ database/helpers/potHoleController.js
@@ -0,0 +1,2 @@
+//this will be where much of the server does its interactions with the pot holes collection
+const PotHole = require('../models/potHoles');
---
+++ database/helpers/userController.js
@@ -0,0 +1,2 @@
+//this is the user helper function that will be where the server makes its interactions with the users collection for the time being
+const User = require('../models/users');
