8ee60f0dc5982f2d209e0f4f9c8678b54ebbcfcc
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-09-11 12:51:24 -0500

    added the save function to the pothole controller, will need to do some checks before saving

--- database/helpers/potHoleController.js
+++ database/helpers/potHoleController.js
@@ -1,2 +1,24 @@
 //this will be where much of the server does its interactions with the pot holes collection
 const PotHole = require('../models/potHoles');
+
+
+module.exports.addPotHoleMarker = function ({ lat, long, rating_mean, last_reported, reported_count, photos, user}, callback){
+
+  const potHole = new PotHole({
+    lat,
+    long,
+    rating_mean,
+    reported_count,
+    last_reported,
+    photos,
+    users,
+  });
+
+  potHole.save(err=>{
+    if(err){
+      console.error(err);
+    }
+    callback(err);
+  })
+
+};
