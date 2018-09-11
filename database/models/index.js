c204a757df60efc69c694ae2863fe71889045193
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-09-11 12:13:48 -0500

    seperated the two schemas into seperate files and put them both into a models folder

--- database/models/index.js
+++ database/models/index.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 const Schema = new mongoose.Schema;
 
-const PotHoleRecords = new Schema({
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('we\'re connected!');
+});
+
+const potHoleRecords = new Schema({
   lat: Number,
   long: Number,
   rating_mean: Number,
@@ -12,7 +19,6 @@ const PotHoleRecords = new Schema({
   users: [String]
 });
 
-const Users = new Schema({
-  username: String,
-  report_count: Number,
-});
+const PotHoles = mongoose.model('Potholes', potHoleRecords);
+
+module.exports = PotHoles;
---
+++ database/models/users.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+const Schema = new mongoose.Schema;
+
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('we\'re connected!');
+});
+
+const users = new Schema({
+  username: String,
+  report_count: Number,
+});
+
+const User = mongoose.model('User', users);
+
+module.exports = User;
