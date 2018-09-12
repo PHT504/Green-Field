dbcd8b36c044801ca93404e64855f7ac38e94a38
Connor <jmconnor3@gmail.com>
2018-09-12 17:23:47 -0500

    'added and then deleted ejs variable'

--- server/index.js
+++ server/index.js
@@ -3,11 +3,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
+app.set("views", "client"); // specify the views directory
+app.set("view engine", "ejs"); 
+app.get('/', (req, res) => {
+res.render('login');
+})
 /*
 POST SUBMIT
 submit route that takes info from client and saves photo and geolocation to database
 
 */
+
 
 app.listen('3000', () => console.log('listening on 3000'));
 module.exports = app;
