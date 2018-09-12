255969cbb116b5503b6da8097598ac33220fed3e
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-09-12 16:11:40 -0500

    the linter yesterday was not upset at me, to day it was, refactored rusercontroller to make linter happy

--- database/helpers/userController.js
+++ database/helpers/userController.js
@@ -1,35 +1,36 @@
-//this is the user helper function that will be where the server makes its interactions with the users collection for the time being
+// this is the user helper function that will be where the server makes its interactions with the users collection for the time being
 const User = require('../models/users');
 
-module.exports.addUser = function ({ username }, callback){
+module.exports.addUser = function addUser({ username }, callback) {
   const user = new User({
     username,
-    report_count: 0
+    report_count: 0,
   });
 
-  user.save(err =>{
-    if(err){
+  user.save((err) => {
+    if (err) {
       console.error(err);
     }
     callback(err);
-  })
+  });
 };
 
-module.exports.selectUser = function (username, callback){
-
-  const query = User.where({username});
+module.exports.selectUser = function selectUser(username, callback) {
+  const query = User.where({ username });
   query.findOne((err, res) => {
-    if(err){
+    if (err) {
       console.error(err);
       callback(err, res);
+    } else if (res === null) {
+      callback('not found', res);
     } else {
-      if(res === null){
-        callback('not found', res);
-      } else {
-        callback(undefined, res);
-      }
+      callback(undefined, res);
     }
   });
-  
+};
 
+// create a way to update the users report count attribute
+
+module.exports.updateUserReportCount = function updateUserReportCount(username, callback) {
+  const query = User.where({ username });
 };
