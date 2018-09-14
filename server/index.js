b9155f128ff9baed889398ad8839d553742f0498
Connor <jmconnor3@gmail.com>
2018-09-14 14:40:53 -0500

    'added frontend and have it connected to express server'

--- server/index.js
+++ server/index.js
@@ -1,22 +1,32 @@
 const express = require('express');
+const fs = require('fs');
 const bodyParser = require('body-parser');
+// const passport = require('passport');
+const { createBundleRenderer } = require('vue-server-renderer');
+const bundleRenderer = createBundleRenderer(
+  require('../frontend/dist/vue-ssr-bundle.json'),
+  {
+    template: fs.readFileSync('./index.html', 'utf-8'),
+  },
+);
 
 const app = express();
 app.use(bodyParser.json());
-app.set("views", "client"); // specify the views directory
-app.set("view engine", "ejs"); 
+app.use('/dist', express.static('dist'));
 
+// app.set('views', 'frontend'); // specify the views directory
+// app.set('view engine', 'ejs');
 /*
 LOGIN ***** PRE AUTHENTICATION
 */
-app.get('/', (req, res) => {
-res.render('login');
-})
+// app.post('/login', (req, res) => {
 
-
-///DURING THE SESSION
-
-
+// });
+app.get('*', (req, res) => {
+  bundleRenderer.renderToStream({ url: req.path })
+    .pipe(res);
+});
+// /DURING THE SESSION
 /*
 POST SUBMIT
 submit route that takes info from client and saves photo and geolocation to database
