6dbb53726795ac076466906182f9ae916fd7f4e5
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-09-14 14:22:47 -0500

    working on figuring out how sessions work. idea i had was adding an access to the session that is true when logged in and false if not or unable to.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const parseurl = require('parseurl');
 const bcrypt = require('bcryptjs');
 const session = require('express-session');
-const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+
+// const cookieParser = require('cookie-parser');
 const UserDB = require('../database/helpers/userController');
 const PotHoleDB = require('../database/helpers/potHoleController');
+const options = require('../database/models/sessionStore');
 
 const app = express();
-app.use(cookieParser());
+// after reading the notes on express-session, it says cookie parser is no longer needed
+// app.use(cookieParser());
+app.use(session({
+  secret: 'find my p hole',
+  saveUninitialized: false,
+  cookie: { maxAge: 60000 },
+  rolling: true,
+  store: new MongoStore({ mongooseConnection: options.connection }),
+}));
+app.use((req, res, next) => {
+  if (!req.session.views) {
+    req.session.views = {};
+  }
+
+  // get the url pathname
+  const { pathname } = parseurl(req);
+
+  // count the views
+  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1;
+
+  next();
+});
 app.use(bodyParser.json());
 app.set('views', 'client'); // specify the views directory
 app.set('view engine', 'ejs');
@@ -40,10 +65,13 @@ app.post('/login', (req, res) => {
   UserDB.selectUser(req.body, (err, result) => {
     if (err) {
       console.error(err);
+      req.session.access = false;
       res.sendStatus(500);
     } else if (bcrypt.compareSync(req.body.password, result.password)) {
+      req.session.access = true;
       res.sendStatus(200);
     } else {
+      req.session.access = false;
       res.sendStatus(403);
     }
   });
