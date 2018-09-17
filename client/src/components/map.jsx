28fcd883dd2eb5f23902932cbeae75c6ba4629b0
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-09-17 14:54:24 -0500

    fixed bundle issue

--- client/src/components/map.jsx
+++ client/src/components/map.jsx
@@ -4,7 +4,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: ''
+    googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA6drgghedMQixurnycyOqoO45qfyNiDzM',
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
