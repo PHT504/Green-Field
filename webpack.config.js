4580a361426bb42b3267a1c48c0f43216a9d3518
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-09-17 11:43:50 -0500

    trying to solve my enviromental issues

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,26 @@ const path = require('path');
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
-module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015'],
+module.exports = (env) => {
+  console.log('NODE_ENV: ', env.NODE_ENV); // 'local'
+  console.log('Production: ', env.production); // true
+  return {
+    entry: `${SRC_DIR}/index.jsx`,
+    output: {
+      filename: 'bundle.js',
+      path: DIST_DIR,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/,
+          include: SRC_DIR,
+          loader: 'babel-loader',
+          query: {
+            presets: ['react', 'es2015'],
+          },
         },
-      },
-    ],
-  },
+      ],
+    },
+  };
 };
