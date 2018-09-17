5a22e9e8ea58e124372712fad5deb78b8256167b
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-09-17 12:26:24 -0500

    using a browser key now hopefully this won't screw me

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,22 @@ const path = require('path');
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
-module.exports = (env) => {
-  console.log('MAP_API: ', env.MAP_API);
-  console.log('Production: ', env.production);
-  return {
-    entry: `${SRC_DIR}/index.jsx`,
-    output: {
-      filename: 'bundle.js',
-      path: DIST_DIR,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?/,
-          include: SRC_DIR,
-          loader: 'babel-loader',
-          query: {
-            presets: ['react', 'es2015'],
-          },
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
         },
-      ],
-    },
-  };
+      },
+    ],
+  },
 };
