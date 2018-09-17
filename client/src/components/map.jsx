d64ede36265bb0d2cd49196a75352df2efaa361f
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-09-16 20:32:40 -0500

    cleaned up for production server

--- client/src/components/map.jsx
+++ client/src/components/map.jsx
@@ -4,7 +4,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAWaI8cSqRLwvcJtu4hbDZvK0c9b7EnM8Y',
+    googleMapURL: process.env.MAP_API,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
